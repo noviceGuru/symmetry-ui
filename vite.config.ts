fdb151e15051ff76ba6451b1475ee809ff5cef76
noviceGuru <amin.arabi@gmail.com>
2024-01-03 08:03:05 +0100

    project set up

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import path from "path"
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    resolve: {
+        alias: {
+            "components": path.resolve(__dirname, "src/components"),
+            "assets": path.resolve(__dirname, "src/assets"),
+            "features": path.resolve(__dirname, "src/features"),
+        },
+    },
+    build: {
+        outDir: "./build",
+    },
+    base: "/pomodoro/",
+})
