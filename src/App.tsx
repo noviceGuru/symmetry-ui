4894fcd385b8b919226a51c25480b1bd4d7a83f9
noviceGuru <amin.arabi@gmail.com>
2024-01-04 15:13:49 +0100

    setting button added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import ClearCompareTranslateButtons from "components/molecules/clear-compare-tra
 import SourceArticel from "components/orgamisms/source-article"
 import TargetArticle from "components/orgamisms/target-article"
 
+import SettingsIcon from "assets/settings-icon.svg"
+
 function App() {
     return (
         <div className="flex flex-col">
-            <div className="flex justify-center flex-1 gap-8 px-8 pt-8 flex-wrap">
+            <Button onClick={()=>alert('settings')} className="w-14 md:absolute mt-3 ml-3 left-3 top-5 rounded-full">
+                <img src={SettingsIcon} alt="settingsIcon"/>
+            </Button>
+            <div className="flex justify-center flex-1 gap-16 px-8 md:px-20 pt-8 flex-wrap">
                 <SourceArticel />
                 <TargetArticle />
             </div>
